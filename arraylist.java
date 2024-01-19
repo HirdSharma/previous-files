fd78ceae82527d0ade9151f1dbd86c6b3473707a
HirdSharma <142671409+HirdSharma@users.noreply.github.com>
2024-01-19 22:04:40 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class arraylist{
+public static void main(String args[]){
+     ArrayList<Integer> list=new ArrayList<>();
+     list.add(0);
+     list.add(1);list.add(4);
+     System.out.println(list);
+     ArrayList<ArrayList<Integer>> list2=new ArrayList<>();
+     ArrayList<Integer> list3=new ArrayList<>();
+     list3.add(2);list3.add(5);list3.add(7);
+     list2.add(list);
+     list2.add(list3);
+     System.out.println(list2);
+     ArrayList<Integer> currlist=
+}
+}
